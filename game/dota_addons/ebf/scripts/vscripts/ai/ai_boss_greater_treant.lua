cdc2291ffb3f8b725a69014498a8b7fe5fbb4785
houthakkerr <y.slagmeulder@hotmail.com>
2024-01-20 13:55:40 +0100

    treant wave tweaks

--- game/dota_addons/ebf/scripts/vscripts/ai/ai_boss_greater_treant.lua
+++ game/dota_addons/ebf/scripts/vscripts/ai/ai_boss_greater_treant.lua
@@ -14,7 +14,7 @@ function Spawn( entityKeyValues )
 		end
 	end)
 	
-	-- thisEntity.shot = thisEntity:FindAbilityByName("boss_treant_seed_shot")
+	thisEntity.shot = thisEntity:FindAbilityByName("boss_treant_seed_shot")
 	
 	Timers:CreateTimer(0.1, function()
 		-- thisEntity.shot:SetLevel(GameRules.gameDifficulty)
@@ -23,18 +23,18 @@ end
 
 function AIThink(thisEntity)
 	if thisEntity:GetTeamNumber() ~= DOTA_TEAM_GOODGUYS and not thisEntity:IsChanneling() and not thisEntity:GetCurrentActiveAbility() then
-		-- if thisEntity.shot:IsFullyCastable() then
-			-- target = thisEntity:FindRandomEnemyInRadius( thisEntity:GetAbsOrigin(), thisEntity.shot:GetTrueCastRange() )
-			-- if target then
-				-- ExecuteOrderFromTable({
-					-- UnitIndex = thisEntity:entindex(),
-					-- OrderType = DOTA_UNIT_ORDER_CAST_TARGET,
-					-- AbilityIndex = thisEntity.shot:entindex(),
-					-- TargetIndex = target:entindex(),
-				-- })
-				-- return 0.1
-			-- end
-		-- end
+		if thisEntity.shot:IsFullyCastable() then
+			target = thisEntity:FindRandomEnemyInRadius( thisEntity:GetAbsOrigin(), thisEntity.shot:GetTrueCastRange() )
+			if target then
+				ExecuteOrderFromTable({
+					UnitIndex = thisEntity:entindex(),
+					OrderType = DOTA_UNIT_ORDER_CAST_TARGET,
+					AbilityIndex = thisEntity.shot:entindex(),
+					TargetIndex = target:entindex(),
+				})
+				return 0.1
+			end
+		end
 		-- no spells left to be cast and not currently attacking
 		if not thisEntity:IsAttacking() then
 			local target = AICore:NearestEnemyHeroInRange( thisEntity, -1, true)

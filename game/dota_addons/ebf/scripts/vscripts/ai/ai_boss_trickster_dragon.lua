f049e22ebe5f2dcfb8056a1a7748e17b0794a3e1
houthakkerr <y.slagmeulder@hotmail.com>
2024-05-08 13:38:43 +0200

    trickster dragon

---
+++ game/dota_addons/ebf/scripts/vscripts/ai/ai_boss_trickster_dragon.lua
@@ -0,0 +1,91 @@
+--[[
+Broodking AI
+]]
+
+
+function Spawn( entityKeyValues )
+	if not IsServer() then
+		return
+	end
+	
+	Timers:CreateTimer(function()
+		if thisEntity and not thisEntity:IsNull() and thisEntity:IsAlive() then
+			return AIThink(thisEntity)
+		end
+	end)
+	
+	thisEntity.orb = thisEntity:FindAbilityByName("boss_trickster_dragon_illusory_orb")
+	thisEntity.rift = thisEntity:FindAbilityByName("boss_trickster_dragon_waning_rift")
+	thisEntity.shift = thisEntity:FindAbilityByName("boss_trickster_dragon_phase_shift")
+	thisEntity.coil = thisEntity:FindAbilityByName("boss_trickster_dragon_dream_coil")
+	thisEntity.duplicate = thisEntity:FindAbilityByName("boss_trickster_dragon_ethereal_duplicate")
+	
+	thisEntity:AddNewModifier( thisEntity, nil, "modifier_item_aghanims_shard", {} )
+	Timers:CreateTimer(0.1, function()
+		thisEntity.orb:SetLevel(GameRules.gameDifficulty)
+		thisEntity.rift:SetLevel(GameRules.gameDifficulty)
+		thisEntity.shift:SetLevel(GameRules.gameDifficulty)
+		thisEntity.coil:SetLevel(GameRules.gameDifficulty)
+		thisEntity.duplicate:SetLevel(GameRules.gameDifficulty)
+	end)
+end
+
+function AIThink(thisEntity)
+	if thisEntity:IsChanneling() and RollPercentage( 25 ) then -- end phase shift whenever
+		thisEntity:Stop()
+	end
+	if thisEntity:GetTeamNumber() ~= DOTA_TEAM_GOODGUYS and not thisEntity:IsChanneling() and not thisEntity:GetCurrentActiveAbility() then
+		if thisEntity.rift:IsFullyCastable() and RollPercentage( 35 ) then
+			local castPosition = AICore:FindOptimalRadiusInRangeForEntity( thisEntity, thisEntity.rift:GetTrueCastRange(), thisEntity.rift:GetAOERadius(), nil, true )
+			if castPosition then
+				ExecuteOrderFromTable({
+					UnitIndex = thisEntity:entindex(),
+					OrderType = DOTA_UNIT_ORDER_CAST_POSITION,
+					Position = castPosition,
+					AbilityIndex = thisEntity.rift:entindex()
+				})
+				return thisEntity.rift:GetCastPoint() + 0.1
+			end
+		end
+		if thisEntity.coil:IsFullyCastable() and RollPercentage( 35 ) then
+			local castPosition = AICore:FindOptimalRadiusInRangeForEntity( thisEntity, thisEntity.coil:GetTrueCastRange(), thisEntity.coil:GetAOERadius(), nil, true )
+			if castPosition then
+				ExecuteOrderFromTable({
+					UnitIndex = thisEntity:entindex(),
+					OrderType = DOTA_UNIT_ORDER_CAST_POSITION,
+					Position = castPosition,
+					AbilityIndex = thisEntity.coil:entindex()
+				})
+				return thisEntity.coil:GetCastPoint() + 0.1
+			end
+		end
+		if thisEntity.orb:IsFullyCastable() and RollPercentage( 35 ) then
+			local castPosition = AICore:FindOptimalLineInRangeForEntity( thisEntity, thisEntity.coil:GetSpecialValueFor("max_distance"), thisEntity.coil:GetSpecialValueFor("radius"), thisEntity.coil:GetSpecialValueFor("max_distance") )
+			if castPosition then
+				ExecuteOrderFromTable({
+					UnitIndex = thisEntity:entindex(),
+					OrderType = DOTA_UNIT_ORDER_CAST_POSITION,
+					Position = castPosition,
+					AbilityIndex = thisEntity.orb:entindex()
+				})
+				return thisEntity.coil:GetCastPoint() + 0.1
+			end
+		end
+		if thisEntity.shift:IsFullyCastable() and RollPercentage( 25 ) then
+			local units = thisEntity:FindEnemyUnitsInRadius( thisEntity:GetAbsOrigin(), thisEntity:GetAttackRange() + thisEntity.shift:GetSpecialValueFor("shard_attack_range_bonus") )
+			if #units > 0 then
+				ExecuteOrderFromTable({
+					UnitIndex = thisEntity:entindex(),
+					OrderType = DOTA_UNIT_ORDER_CAST_POSITION,
+					Position = castPosition,
+					AbilityIndex = thisEntity.shift:entindex()
+				})
+				return thisEntity.shift:GetCastPoint() + 0.1
+			end
+		end
+		-- no spells left to be cast and not currently attacking
+		return AICore:HandleBasicAI( thisEntity )
+	else 
+		return AI_THINK_RATE 
+	end
+end
--- game/dota_addons/ebf/scripts/vscripts/bosses/boss_puck/boss_trickster_dragon_ethereal_duplicate.lua
+++ game/dota_addons/ebf/scripts/vscripts/bosses/boss_puck/boss_trickster_dragon_ethereal_duplicate.lua
@@ -1,1 +1,79 @@
 boss_trickster_dragon_ethereal_duplicate = class({})
+
+function boss_trickster_dragon_ethereal_duplicate:GetIntrinsicModifierName()
+	return "modifier_boss_trickster_dragon_ethereal_duplicate"
+end
+
+modifier_boss_trickster_dragon_ethereal_duplicate = class({})
+LinkLuaModifier( "modifier_boss_trickster_dragon_ethereal_duplicate", "bosses/boss_puck/boss_trickster_dragon_ethereal_duplicate", LUA_MODIFIER_MOTION_NONE )
+
+function modifier_boss_trickster_dragon_ethereal_duplicate:OnCreated()
+	self:OnRefresh()
+end
+
+function modifier_boss_trickster_dragon_ethereal_duplicate:OnRefresh()
+	self.duration = self:GetSpecialValueFor("duration")
+	self.incoming_damage = self:GetSpecialValueFor("incoming_damage") / 100
+end
+
+function modifier_boss_trickster_dragon_ethereal_duplicate:DeclareFunctions()
+	return {MODIFIER_EVENT_ON_ABILITY_FULLY_CAST}
+end
+
+function modifier_boss_trickster_dragon_ethereal_duplicate:OnAbilityFullyCast( params )
+	if params.unit == self:GetParent() then
+		local illusion = CreateUnitByName( params.unit:GetUnitName(), params.unit:GetAbsOrigin(), true, nil, nil, self:GetCaster():GetTeam() )
+		params.unit:Blink( params.unit:GetAbsOrigin() + RandomVector( 600 ) )
+		illusion:Blink( illusion:GetAbsOrigin() + RandomVector( 600 ) )
+		
+		for i = 0, illusion:GetAbilityCount() - 1 do
+			local ability = illusion:GetAbilityByIndex( i )
+			if ability then
+				ability:SetActivated( false )
+			end
+		end
+		illusion:SetCoreHealth( illusion:GetMaxHealth() / self.incoming_damage )
+		illusion:SetHealth( math.ceil( illusion:GetMaxHealth() * (params.unit:GetHealthPercent() / 100) ) )
+		
+		illusion:AddNewModifier( params.unit, self:GetAbility(), "modifier_boss_trickster_dragon_ethereal_duplicate_illusion", {duration = self.duration} )
+	end
+end
+
+function modifier_boss_trickster_dragon_ethereal_duplicate:IsHidden()
+	return true
+end
+
+modifier_boss_trickster_dragon_ethereal_duplicate_illusion = class({})
+LinkLuaModifier( "modifier_boss_trickster_dragon_ethereal_duplicate_illusion", "bosses/boss_puck/boss_trickster_dragon_ethereal_duplicate", LUA_MODIFIER_MOTION_NONE )
+
+function modifier_boss_trickster_dragon_ethereal_duplicate_illusion:OnCreated()
+	self:OnRefresh()
+end
+
+function modifier_boss_trickster_dragon_ethereal_duplicate_illusion:OnRefresh()
+	self.outgoing_damage = self:GetSpecialValueFor("outgoing_damage") - 100
+end
+
+function modifier_boss_trickster_dragon_ethereal_duplicate_illusion:OnDestroy()
+	if IsServer() and self:GetParent():IsAlive() then
+		self:GetParent():ForceKill( false )
+	end
+end
+
+function modifier_boss_trickster_dragon_ethereal_duplicate_illusion:DeclareFunctions()
+	return {MODIFIER_PROPERTY_DISABLE_HEALING, MODIFIER_PROPERTY_BONUSDAMAGEOUTGOING_PERCENTAGE, MODIFIER_PROPERTY_BASEDAMAGEOUTGOING_PERCENTAGE }
+end
+
+function modifier_boss_trickster_dragon_ethereal_duplicate_illusion:GetDisableHealing()
+	return 1
+end
+function modifier_boss_trickster_dragon_ethereal_duplicate_illusion:GetModifierBonusDamageOutgoing_Percentage()
+	return -100
+end
+function modifier_boss_trickster_dragon_ethereal_duplicate_illusion:GetModifierBaseDamageOutgoing_Percentage()
+	return self.outgoing_damage
+end
+
+function modifier_boss_trickster_dragon_ethereal_duplicate_illusion:IsHidden()
+	return true
+end

e22f77ad017cdb742e43ec470ab12d42d90d57f0
Mayhem <onetimeliving@gmail.com>
2024-06-12 02:58:03 +0300

    Shadow Shaman

--- game/dota_addons/ebf/scripts/vscripts/heroes/hero_shadow_shaman/shadow_shaman_shackles.lua
+++ game/dota_addons/ebf/scripts/vscripts/heroes/hero_shadow_shaman/shadow_shaman_shackles.lua
@@ -1,113 +1,126 @@
 shadow_shaman_shackles = class({})
 
 function shadow_shaman_shackles:GetChannelTime()
-	return self._channelTimeToVisualize
+    return self._channelTimeToVisualize
 end
 
-function shadow_shaman_shackles:UpdateChannelTime( channelTime )
-	self._channelTimeToVisualize = math.max( self._channelTimeToVisualize or 0, channelTime )
+function shadow_shaman_shackles:UpdateChannelTime(channelTime)
+    self._channelTimeToVisualize = math.max(self._channelTimeToVisualize or 0, channelTime)
 end
 
 function shadow_shaman_shackles:OnSpellStart()
-	local hTarget = self:GetCursorTarget()
-	local hCaster = self:GetCaster()
-	
-	-- cosmetic
-	EmitSoundOn("Hero_ShadowShaman.Shackles.Cast", hCaster)
-	self.shackleTargets = self.shackleTargets or {}
-	if not hTarget:TriggerSpellAbsorb( self ) then
-		local mod = hTarget:AddNewModifier(hCaster, self, "modifier_shadow_shaman_bound_shackles", {duration = self:GetSpecialValueFor("channel_time")})
-	end
+    local hCaster = self:GetCaster()
+    local vTargetPosition = self:GetCursorPosition()
+    local radius = self:GetSpecialValueFor("radius")
+    
+    -- cosmetic
+    EmitSoundOn("Hero_ShadowShaman.Shackles.Cast", hCaster)
+    self.shackleTargets = self.shackleTargets or {}
+    
+    local enemies = FindUnitsInRadius(
+        hCaster:GetTeamNumber(),
+        vTargetPosition,
+        nil,
+        radius,
+        DOTA_UNIT_TARGET_TEAM_ENEMY,
+        DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,
+        DOTA_UNIT_TARGET_FLAG_NONE,
+        FIND_ANY_ORDER,
+        false
+    )
+    
+    for _, hTarget in pairs(enemies) do
+        if not hTarget:TriggerSpellAbsorb(self) then
+            local mod = hTarget:AddNewModifier(hCaster, self, "modifier_shadow_shaman_bound_shackles", {duration = self:GetSpecialValueFor("channel_time")})
+        end
+    end
 end
 
 function shadow_shaman_shackles:OnChannelThink()
-	if #self.shackleTargets <= 0 then
-		self:GetCaster():InterruptChannel()
-	end
+    if #self.shackleTargets <= 0 then
+        self:GetCaster():InterruptChannel()
+    end
 end
 
 LinkLuaModifier("modifier_shadow_shaman_bound_shackles", "heroes/hero_shadow_shaman/shadow_shaman_shackles", LUA_MODIFIER_MOTION_NONE)
 modifier_shadow_shaman_bound_shackles = class({})
 
 function modifier_shadow_shaman_bound_shackles:OnCreated(kv)
-	self.duration = self:GetRemainingTime()
-	self.damage = self:GetAbility():GetSpecialValueFor("total_damage")
-	self.heal = self:GetAbility():GetSpecialValueFor("total_heal")
-	self.tick = self:GetAbility():GetSpecialValueFor("tick_interval")
-	EmitSoundOn("Hero_ShadowShaman.Shackles", self:GetParent())
-	if IsServer() then
-		self:StartIntervalThink(self.tick)
-		
-		local origin = self:GetCaster()
-		if kv.origin then origin = EntIndexToHScript( tonumber(kv.origin) ) end
-		local shackles = ParticleManager:CreateParticle("particles/units/heroes/hero_shadowshaman/shadowshaman_shackle.vpcf", PATTACH_POINT_FOLLOW, self:GetParent())
-		ParticleManager:SetParticleControlEnt(shackles, 0, self:GetParent(), PATTACH_POINT_FOLLOW, "attach_hitloc", self:GetParent():GetAbsOrigin(), true)
-		ParticleManager:SetParticleControlEnt(shackles, 1, self:GetParent(), PATTACH_POINT_FOLLOW, "attach_hitloc", self:GetParent():GetAbsOrigin(), true)
-		ParticleManager:SetParticleControlEnt(shackles, 3, self:GetParent(), PATTACH_POINT_FOLLOW, "attach_hitloc", self:GetParent():GetAbsOrigin(), true)
-		ParticleManager:SetParticleControlEnt(shackles, 4, self:GetParent(), PATTACH_POINT_FOLLOW, "attach_hitloc", self:GetParent():GetAbsOrigin(), true)
-		
-		ParticleManager:SetParticleControlEnt(shackles, 5, origin, PATTACH_POINT_FOLLOW, "attach_attack1", origin:GetAbsOrigin(), true)
-		ParticleManager:SetParticleControlEnt(shackles, 6, origin, PATTACH_POINT_FOLLOW, "attach_attack2", origin:GetAbsOrigin(), true)
-		
-		self:AddParticle(shackles, false, false, 10, false, false)
-		
-		
-	end
-	
-	self:GetAbility().shackleTargets = self:GetAbility().shackleTargets or {}
-	table.insert( self:GetAbility().shackleTargets, self )
-	self:GetAbility():UpdateChannelTime( self:GetRemainingTime() )
+    self.duration = self:GetRemainingTime()
+    self.damage = self:GetAbility():GetSpecialValueFor("total_damage")
+    self.heal = self:GetAbility():GetSpecialValueFor("total_heal")
+    self.tick = self:GetAbility():GetSpecialValueFor("tick_interval")
+    EmitSoundOn("Hero_ShadowShaman.Shackles", self:GetParent())
+    if IsServer() then
+        self:StartIntervalThink(self.tick)
+        
+        local origin = self:GetCaster()
+        if kv.origin then origin = EntIndexToHScript(tonumber(kv.origin)) end
+        local shackles = ParticleManager:CreateParticle("particles/units/heroes/hero_shadowshaman/shadowshaman_shackle.vpcf", PATTACH_POINT_FOLLOW, self:GetParent())
+        ParticleManager:SetParticleControlEnt(shackles, 0, self:GetParent(), PATTACH_POINT_FOLLOW, "attach_hitloc", self:GetParent():GetAbsOrigin(), true)
+        ParticleManager:SetParticleControlEnt(shackles, 1, self:GetParent(), PATTACH_POINT_FOLLOW, "attach_hitloc", self:GetParent():GetAbsOrigin(), true)
+        ParticleManager:SetParticleControlEnt(shackles, 3, self:GetParent(), PATTACH_POINT_FOLLOW, "attach_hitloc", self:GetParent():GetAbsOrigin(), true)
+        ParticleManager:SetParticleControlEnt(shackles, 4, self:GetParent(), PATTACH_POINT_FOLLOW, "attach_hitloc", self:GetParent():GetAbsOrigin(), true)
+        
+        ParticleManager:SetParticleControlEnt(shackles, 5, origin, PATTACH_POINT_FOLLOW, "attach_attack1", origin:GetAbsOrigin(), true)
+        ParticleManager:SetParticleControlEnt(shackles, 6, origin, PATTACH_POINT_FOLLOW, "attach_attack2", origin:GetAbsOrigin(), true)
+        
+        self:AddParticle(shackles, false, false, 10, false, false)
+    end
+    
+    self:GetAbility().shackleTargets = self:GetAbility().shackleTargets or {}
+    table.insert(self:GetAbility().shackleTargets, self)
+    self:GetAbility():UpdateChannelTime(self:GetRemainingTime())
 end
 
 function modifier_shadow_shaman_bound_shackles:OnRefresh()
-	self.duration = self:GetRemainingTime()
-	self.damage = self:GetAbility():GetSpecialValueFor("total_damage") / self.duration
-	self.tick = self:GetAbility():GetSpecialValueFor("tick_interval")
-	EmitSoundOn("Hero_ShadowShaman.Shackles", self:GetParent())
+    self.duration = self:GetRemainingTime()
+    self.damage = self:GetAbility():GetSpecialValueFor("total_damage") / self.duration
+    self.tick = self:GetAbility():GetSpecialValueFor("tick_interval")
+    EmitSoundOn("Hero_ShadowShaman.Shackles", self:GetParent())
 end
 
 function modifier_shadow_shaman_bound_shackles:OnIntervalThink()
-	if not self:GetCaster():IsChanneling() then self:Destroy() end
-	local caster = self:GetCaster()
-	local ability = self:GetAbility()
-	local target = self:GetParent()
-	ability:DealDamage( caster, target, self.damage*self.tick )
-	caster:HealEvent( self.heal*self.tick, ability, caster )
-	ApplyDamage({ victim = self:GetParent(), attacker = self:GetCaster(), damage = self.damage*self.tick, damage_type = self:GetAbility():GetAbilityDamageType(), ability = self:GetAbility()})
+    if not self:GetCaster():IsChanneling() then self:Destroy() end
+    local caster = self:GetCaster()
+    local ability = self:GetAbility()
+    local target = self:GetParent()
+    ability:DealDamage(caster, target, self.damage * self.tick)
+    caster:HealEvent(self.heal * self.tick, ability, caster)
+    ApplyDamage({victim = self:GetParent(), attacker = self:GetCaster(), damage = self.damage * self.tick, damage_type = self:GetAbility():GetAbilityDamageType(), ability = self:GetAbility()})
 end
 
 function modifier_shadow_shaman_bound_shackles:OnDestroy()
-	StopSoundOn("Hero_ShadowShaman.Shackles", self:GetParent())
-	if IsServer() then
-		table.removeval(self:GetAbility().shackleTargets, self)
-	end
+    StopSoundOn("Hero_ShadowShaman.Shackles", self:GetParent())
+    if IsServer() then
+        table.removeval(self:GetAbility().shackleTargets, self)
+    end
 end
 
 function modifier_shadow_shaman_bound_shackles:DeclareFunctions()
-	local funcs = {
-		MODIFIER_PROPERTY_OVERRIDE_ANIMATION
-	}
+    local funcs = {
+        MODIFIER_PROPERTY_OVERRIDE_ANIMATION
+    }
 
-	return funcs
+    return funcs
 end
 
 function modifier_shadow_shaman_bound_shackles:GetOverrideAnimation()
-	return ACT_DOTA_DISABLED
+    return ACT_DOTA_DISABLED
 end
 
 function modifier_shadow_shaman_bound_shackles:GetStatusEffectName()
-	return "particles/status_fx/status_effect_shaman_shackle.vpcf"
+    return "particles/status_fx/status_effect_shaman_shackle.vpcf"
 end
 
 function modifier_shadow_shaman_bound_shackles:StatusEffectPriority()
-	return 10
+    return 10
 end
 
-
 function modifier_shadow_shaman_bound_shackles:CheckState()
-	local state = {
-	[MODIFIER_STATE_STUNNED] = true,
-	}
+    local state = {
+        [MODIFIER_STATE_STUNNED] = true,
+    }
 
-	return state
+    return state
 end

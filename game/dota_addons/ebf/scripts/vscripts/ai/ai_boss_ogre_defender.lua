b565ad1653029278829fe3ebc6829eb7535dbafd
Mayhem <onetimeliving@gmail.com>
2024-07-01 18:44:16 +0300

    Ogre wave Animation/AI fixes

--- game/dota_addons/ebf/scripts/vscripts/ai/ai_boss_ogre_defender.lua
+++ game/dota_addons/ebf/scripts/vscripts/ai/ai_boss_ogre_defender.lua
@@ -1,42 +1,36 @@
---[[
-Broodking AI
-]]
+--[[ Ogre Defender AI ]]
 
-
-function Spawn( entityKeyValues )
+function Spawn(entityKeyValues)
 	if not IsServer() then
 		return
 	end
 	
+	thisEntity.cone_push = thisEntity:FindAbilityByName("boss_ogre_defender_cone_push")
+
+	if thisEntity.cone_push then
+		thisEntity.cone_push:ToggleAutoCast()
+	end
+
 	Timers:CreateTimer(function()
 		if thisEntity and not thisEntity:IsNull() and thisEntity:IsAlive() then
 			return AIThink(thisEntity)
 		end
 	end)
-	
-	thisEntity.lust = thisEntity:FindAbilityByName("boss_ogre_tank_bloodlust")
-	thisEntity.multicast = thisEntity:FindAbilityByName("boss_ogre_magi_multicast")
-	
-	Timers:CreateTimer(0.1, function()
-		thisEntity.lust:SetLevel(GameRules.gameDifficulty)
-		thisEntity.multicast:SetLevel(GameRules.gameDifficulty)
-	end)
 end
-
 
 function AIThink(thisEntity)
 	if thisEntity:GetTeamNumber() ~= DOTA_TEAM_GOODGUYS and not thisEntity:IsChanneling() and not thisEntity:GetCurrentActiveAbility() then
-		if thisEntity.lust:IsFullyCastable() and thisEntity:GetAttackTarget() and not thisEntity:HasModifier("modifier_ogre_magi_bloodlust") then
+		if thisEntity.cone_push and thisEntity.cone_push:IsFullyCastable() then
 			ExecuteOrderFromTable({
 				UnitIndex = thisEntity:entindex(),
-				OrderType = DOTA_UNIT_ORDER_CAST_TARGET,
-				AbilityIndex = thisEntity.lust:entindex(),
-				TargetIndex = thisEntity:entindex()
+				OrderType = DOTA_UNIT_ORDER_CAST_NO_TARGET,
+				AbilityIndex = thisEntity.cone_push:entindex()
 			})
-			return 0.1
-		end-- no spells left to be cast and not currently attacking
+			return 1
+		end
+		
 		if not thisEntity:IsAttacking() then
-			local target = AICore:NearestEnemyHeroInRange( thisEntity, -1, true)
+			local target = AICore:NearestEnemyHeroInRange(thisEntity, -1, true)
 			if target and target ~= thisEntity:GetAttackTarget() then
 				ExecuteOrderFromTable({
 					UnitIndex = thisEntity:entindex(),
---
+++ game/dota_addons/ebf/scripts/vscripts/bosses/boss_ogres/boss_ogre_defender_cone_push.lua
@@ -0,0 +1,77 @@
+boss_ogre_defender_cone_push = class({})
+
+function boss_ogre_defender_cone_push:OnAttackLanded(params)
+	if params.attacker == self:GetCaster() and self:GetAutoCastState() and self:IsCooldownReady() and self:GetCaster():GetMana() >= self:GetManaCost(self:GetLevel()) then
+		self:CastAbility()
+	end
+end
+
+function boss_ogre_defender_cone_push:OnSpellStart()
+	local caster = self:GetCaster()
+	local ability = self
+	local cone_angle = ability:GetSpecialValueFor("cone_angle")
+	local cone_distance = ability:GetSpecialValueFor("cone_distance")
+	local pushback_distance = ability:GetSpecialValueFor("pushback_distance")
+
+	local caster_position = caster:GetAbsOrigin()
+	local caster_forward = caster:GetForwardVector()
+
+	-- Play sound effect
+	EmitSoundOn("Hero_Tiny.Tree.Grab", caster)
+
+	-- Create particle effect
+	local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_tiny/tiny_tree_attack.vpcf", PATTACH_ABSORIGIN_FOLLOW, caster)
+	ParticleManager:SetParticleControl(particle, 0, caster_position)
+	ParticleManager:SetParticleControlForward(particle, 0, caster_forward)
+	ParticleManager:ReleaseParticleIndex(particle)
+
+	local enemies = FindUnitsInRadius(
+		caster:GetTeamNumber(),
+		caster_position,
+		nil,
+		cone_distance,
+		DOTA_UNIT_TARGET_TEAM_ENEMY,
+		DOTA_UNIT_TARGET_HERO,
+		DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES,
+		FIND_ANY_ORDER,
+		false
+	)
+
+	for _, enemy in pairs(enemies) do
+		local enemy_position = enemy:GetAbsOrigin()
+		local direction = (enemy_position - caster_position):Normalized()
+		local angle = math.deg(math.acos(caster_forward:Dot(direction)))
+
+		if angle <= cone_angle / 2 then
+			-- Push back
+			local push_direction = direction
+			enemy:SetAbsOrigin(enemy_position + push_direction * pushback_distance)
+		end
+	end
+
+	-- Spend mana and trigger cooldown
+	caster:SpendMana(self:GetManaCost(self:GetLevel()), self)
+	self:StartCooldown(self:GetCooldown(self:GetLevel()))
+end
+
+function boss_ogre_defender_cone_push:OnUpgrade()
+	if IsServer() then
+		self:GetCaster():AddNewModifier(self:GetCaster(), self, "modifier_boss_ogre_defender_cone_push_autocast", {})
+	end
+end
+
+LinkLuaModifier("modifier_boss_ogre_defender_cone_push_autocast", "bosses/boss_ogres/boss_ogre_defender_cone_push.lua", LUA_MODIFIER_MOTION_NONE)
+
+modifier_boss_ogre_defender_cone_push_autocast = class({})
+
+function modifier_boss_ogre_defender_cone_push_autocast:IsHidden() return true end
+function modifier_boss_ogre_defender_cone_push_autocast:IsPurgable() return false end
+function modifier_boss_ogre_defender_cone_push_autocast:DeclareFunctions()
+	return { MODIFIER_EVENT_ON_ATTACK_LANDED }
+end
+
+function modifier_boss_ogre_defender_cone_push_autocast:OnAttackLanded(params)
+	if IsServer() then
+		self:GetAbility():OnAttackLanded(params)
+	end
+end

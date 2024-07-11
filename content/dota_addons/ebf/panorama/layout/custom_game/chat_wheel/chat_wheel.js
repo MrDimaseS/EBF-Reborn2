287a59905ec9113cde686487c8a437c48194dce0
Mayhem <onetimeliving@gmail.com>
2024-07-11 22:55:32 +0300

    Chat Wheel fix #1

--- content/dota_addons/ebf/panorama/layout/custom_game/chat_wheel/chat_wheel.js
+++ content/dota_addons/ebf/panorama/layout/custom_game/chat_wheel/chat_wheel.js
@@ -6,26 +6,6 @@ GameUI.ToggleSingleClassInParent = (parent, child, class_name) => {
     if (child) child.AddClass(class_name);
 };
 
-let ToggleVoiceChatShow = () => {
-    $.GetContextPanel().ToggleClass("Show_VC");
-    if (!$.GetContextPanel().BHasClass("Show_VC")) $.DispatchEvent("DropInputFocus");
-
-    if ($.GetContextPanel().BHasClass("Show_VC")) {
-        state = 1;
-        mouseCallback = function (eventName, button) {
-            if (eventName === "pressed" && button === 0) {
-                state = 0;
-                $.GetContextPanel().RemoveClass("Show_VC");
-                GameUI.SetMouseCallback(null);
-            }
-        };
-        GameUI.SetMouseCallback(mouseCallback);
-        updateChatWheelLines(); // Add this line
-    }
-};
-
-
-
 GameUI.VoiceChat.Show = () => {
     $.GetContextPanel().AddClass("Show_VC");
 };
@@ -36,50 +16,50 @@ function OnPhraseSelected(phraseId) {
     if (button && !button.BHasClass("Disabled")) {
         const playerName = Game.GetLocalPlayerInfo().player_name;
         const soundNames = {
-        1: "Lakad Matataaag! Normalin, Normalin.",
-        2: "The next level play!",
-		3: "А ну-ка иди-ка сюда. А вот все. Все.",
-		4: "Absolutely Perfect",
-		5: "They're all dead!",
-		6: "唉唉唉！唉？唉 …",
-		7: "Brutal. Savage. Rekt",
-		8: "Ceeeeeeeeeb!",
-		9: "Что это?! Какая жесть!",
-		10: "Crash and burn",
-		11: "Crowd Groan",
-		12: "Сrybaby",
-		13: "Ding Ding Ding Mother******",
-		14: "It's a disastah!",
-		15: "啊，队友呢？队友呢？队友呢？！队友呢？！？！",
-		16: "Easiest money of my life!",
-		17: "Echo Slamma Jamma!",
-		18: "Это ГГ",
-		19: "Это уже попахивает фидом",
-		20: "Это. Просто. Нечто.",
-		21: "Holy Moly!",
-		22: "10억짜리 꿈의 고리! 15억짜리 꿈의 고리!",
-		23: "Резать Резать Резать Резать",
-		24: "你行你行，你上你上",
-		25: "This guy has no chill",
-		26: "Ouch",
-		27: "Ой-ой-ой-ой-ой, бежать!",
-		28: "Patience from Zhou",
-		29: "See you later nerds",
-		30: "Monkey Business",
-		31: "What the **** just happened?",
-		32: "Wow!",
-		33: "Ай-ай-ай-ай-ай, что сейчас произошло!"
-    };
-    const soundName = soundNames[phraseId];
+            1: "Lakad Matataaag! Normalin, Normalin.",
+            2: "The next level play!",
+            3: "А ну-ка иди-ка сюда. А вот все. Все.",
+            4: "Absolutely Perfect",
+            5: "They're all dead!",
+            6: "唉唉唉！唉？唉 …",
+            7: "Brutal. Savage. Rekt",
+            8: "Ceeeeeeeeeb!",
+            9: "Что это?! Какая жесть!",
+            10: "Crash and burn",
+            11: "Crowd Groan",
+            12: "Сrybaby",
+            13: "Ding Ding Ding Mother******",
+            14: "It's a disastah!",
+            15: "啊，队友呢？队友呢？队友呢？！队友呢？！？！",
+            16: "Easiest money of my life!",
+            17: "Echo Slamma Jamma!",
+            18: "Это ГГ",
+            19: "Это уже попахивает фидом",
+            20: "Это. Просто. Нечто.",
+            21: "Holy Moly!",
+            22: "10억짜리 꿈의 고리! 15억짜리 꿈의 고리!",
+            23: "Резать Резать Резать Резать",
+            24: "你行你行，你上你上",
+            25: "This guy has no chill",
+            26: "Ouch",
+            27: "Ой-ой-ой-ой-ой, бежать!",
+            28: "Patience from Zhou",
+            29: "See you later nerds",
+            30: "Monkey Business",
+            31: "What the **** just happened?",
+            32: "Wow!",
+            33: "Ай-ай-ай-ай-ай, что сейчас произошло!"
+        };
+        const soundName = soundNames[phraseId];
 
-    // Localize the sound name
-	const localizedSoundName = $.Localize(`${soundName}`);
+        // Localize the sound name
+        const localizedSoundName = $.Localize(`${soundName}`);
 
-	GameEvents.SendCustomGameEventToServer("emit_sound_for_all_players", { phraseId: phraseId, playerName: playerName, soundName: localizedSoundName });
+        GameEvents.SendCustomGameEventToServer("emit_sound_for_all_players", { phraseId: phraseId, playerName: playerName, soundName: localizedSoundName });
+    }
 }
-}
 
-function disableChatWheelLinesBasedOnMMR(mmr) {
+function disableChatWheelLinesBasedOnMMR(playerMMR) {
     const chatWheelLines = [
         { id: 1, mmr: 6000 },
         { id: 2, mmr: 5500 },
@@ -102,7 +82,7 @@ function disableChatWheelLinesBasedOnMMR(mmr) {
 
         const elements = [mmrLabel, leftButton, midButton, rightButton];
 
-        if (mmr < line.mmr) {
+        if (playerMMR < line.mmr) {
             elements.forEach(el => {
                 if (el) {
                     el.AddClass("Disabled");
@@ -117,7 +97,7 @@ function disableChatWheelLinesBasedOnMMR(mmr) {
                     el.RemoveClass("Disabled");
                     if (el.id.startsWith("text")) {
                         const phraseId = parseInt(el.id.replace("text", ""));
-                        el.SetPanelEvent("onactivate", function() { OnPhraseSelected(phraseId); });
+                        el.SetPanelEvent("onactivate", function () { OnPhraseSelected(phraseId); });
                     }
                 }
             });
@@ -125,25 +105,54 @@ function disableChatWheelLinesBasedOnMMR(mmr) {
     });
 }
 
-function updateChatWheelLines() {
-    const mmrData = CustomNetTables.GetTableValue("mmr", "0");
-    if (mmrData && mmrData.mmr) {
-        disableChatWheelLinesBasedOnMMR(mmrData.mmr);
+function GetCurrentMMRForPlayer(playerId) {
+    const netTable = CustomNetTables.GetTableValue("mmr", playerId.toString());
+    if (netTable != undefined && netTable.mmr != undefined) {
+        return netTable.mmr;
     } else {
-        $.Msg("MMR data not found");
+        return 0; // Return 0 instead of "N/A" for easier comparison
     }
 }
+
+function updateChatWheelLines() {
+    const playerID = Game.GetLocalPlayerID();
+    const mmr = GetCurrentMMRForPlayer(playerID);
+
+    if (mmr !== 0) {
+        $.Msg(`Updating chat wheel lines for player ${playerID} with MMR: ${mmr}`);
+        disableChatWheelLinesBasedOnMMR(mmr);
+    } else {
+        $.Msg(`MMR data not found for player ${playerID}`);
+        disableChatWheelLinesBasedOnMMR(0); // Disable all lines if MMR is not available
+    }
+}
+
+let ToggleVoiceChatShow = () => {
+    $.GetContextPanel().ToggleClass("Show_VC");
+    if (!$.GetContextPanel().BHasClass("Show_VC")) $.DispatchEvent("DropInputFocus");
+
+    if ($.GetContextPanel().BHasClass("Show_VC")) {
+        state = 1;
+        mouseCallback = function (eventName, button) {
+            if (eventName === "pressed" && button === 0) {
+                state = 0;
+                $.GetContextPanel().RemoveClass("Show_VC");
+                GameUI.SetMouseCallback(null);
+            }
+        };
+        GameUI.SetMouseCallback(mouseCallback);
+        updateChatWheelLines();
+    }
+};
 
 (() => {
     GameUI.Custom_ToggleVoiceChat = ToggleVoiceChatShow;
 
     CustomNetTables.SubscribeNetTableListener("mmr", (tableName, key, data) => {
-        if (key === "0" && data && data.mmr) {
+        const playerID = Game.GetLocalPlayerID();
+        if (key === playerID.toString() && data && data.mmr !== undefined) {
+            $.Msg(`MMR updated for player ${playerID}: ${data.mmr}`);
             updateChatWheelLines();
         }
     });
-})();
-
-(() => {
-    GameUI.Custom_ToggleVoiceChat = ToggleVoiceChatShow;
 })();

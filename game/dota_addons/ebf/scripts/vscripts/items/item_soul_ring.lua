1b1ed9dcfdf520a341918011d05b84315d54bd02
houthakkerr <y.slagmeulder@hotmail.com>
2025-08-24 15:12:57 +0200

    health rescalin done

--- game/dota_addons/ebf/scripts/vscripts/items/item_soul_ring.lua
+++ game/dota_addons/ebf/scripts/vscripts/items/item_soul_ring.lua
@@ -59,7 +59,7 @@ end
 function modifier_item_soul_ring_toggle:OnRefresh()
 	self.max_hp_loss = self:GetSpecialValueFor("max_hp_loss") / 100
 	self.max_hp_loss_tick = self:GetSpecialValueFor("max_hp_loss_tick")
-	-- self.loss_to_gain = self:GetSpecialValueFor("loss_to_gain")
+	self.loss_to_gain = self:GetSpecialValueFor("loss_to_gain")
 	self.mana_gain = self:GetSpecialValueFor("mana_gain")
 	self.mana_gain_radius = self:GetSpecialValueFor("mana_gain_radius")
 end
@@ -74,13 +74,9 @@ function modifier_item_soul_ring_toggle:OnIntervalThink()
 	ParticleManager:FireParticle("particles/items2_fx/soul_ring.vpcf", PATTACH_POINT_FOLLOW, parent )
 	EmitSoundOn( "LotusPool.Target", parent )
 	
-	-- old mana gain based on hp loss
-	-- local manaGain = (self._manaGainLeftOver or 0) + (damageTaken / self.loss_to_gain) * self.mana_gain
-	-- self._manaGainLeftOver = manaGain % 1
-	-- print( manaGain, self._manaGainLeftOver )
-
-	-- new flat mana gain
-	local manaGain = self.mana_gain
+	old mana gain based on hp loss
+	local manaGain = (self._manaGainLeftOver or 0) + (damageTaken / self.loss_to_gain) * self.mana_gain
+	self._manaGainLeftOver = manaGain % 1
 
 	for _, ally in ipairs( parent:FindFriendlyUnitsInRadius( parent:GetAbsOrigin(), self.mana_gain_radius ) ) do
 		ally:GiveMana( manaGain )

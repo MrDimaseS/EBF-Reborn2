b1c82b34000fd2f497979206386930b916095c2d
Shijuu <deltapotatoes@gmail.com>
2025-09-13 15:28:28 +0800

    Revert "Juggernaut Rework"
    This reverts commit 7e7a6ef332cf0bd1713e112df88dce01d7aaa329.

--- game/dota_addons/ebf/scripts/vscripts/epic_boss_fight_game_round.lua
+++ game/dota_addons/ebf/scripts/vscripts/epic_boss_fight_game_round.lua
@@ -159,11 +159,6 @@ function CHoldoutGameRound:End(bWon)
 			spawner:End()
 		end
 	end
-	for _,illusion in pairs (self:FindAllUnits()) do
-		if illusion:HasModifier("modifier_illusion") or illusion:IsIllusion() == true then
-			illusion:ForceKill(false)
-		end
-	end
 	GameRules._processValuesForScaling = nil
 end
 
